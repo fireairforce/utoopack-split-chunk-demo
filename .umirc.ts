eed367d60b500269d534f5b1c6f455621b67e0ad
zoomdong <1344492820@qq.com>
2026-01-14 16:20:02 +0800

    chore: init project

---
+++ .umirc.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/detail", component: "detail" },
+    { path: "/detail2", component: "detail2" },
+    { path: "/detail3", component: "detail3" },
+  ],
+  plugins: ['@umijs/plugins/dist/antd', '@umijs/plugins/dist/locale'],
+  locale: {
+    title: true,
+    default: 'zh-CN',
+  },
+  antd: {
+    // valid for antd5.0 only
+    theme: {
+      token: {
+        colorPrimary: '#1DA57A',
+      },
+    },
+    styleProvider: {
+      hashPriority: 'high',
+      legacyTransformer: true,
+    },
+    // dark: true,
+    compact: true,
+    /**
+     * antd@5.1.0 ~ 5.2.3 仅支持 appConfig: {}, 来启用 <App /> 组件;
+     * antd@5.3.0 及以上才支持 appConfig: { // ... } 来添加更多 App 配置项;
+     */
+    appConfig: {
+      message: {
+        maxCount: 3,
+      },
+    },
+  },
+  npmClient: 'pnpm',
+});
